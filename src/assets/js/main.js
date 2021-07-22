2a1224dbedf0d053445191496b43c322a78cd2df
wataru <wtriijm0331@gmail.com>
2021-07-22 14:46:21 +0900

    link追加

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,4 +2,5 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite Starter!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <a href="/dir1/">Dir1</a>
+  <a href="/dir2/">Dir2</a>
 `
